d43dae7174560d278dc990cc1fe2fc82407709fe
dendo1234 <37016869+dendo1234@users.noreply.github.com>
2024-04-26 23:47:23 +0000

    cap 4.8

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,4 +24,25 @@ class MoviesController < ApplicationController
         # rest of code...
       end
 
+      def edit
+        @movie = Movie.find params[:id]
+      end
+      
+      def update
+        @movie = Movie.find params[:id]
+        permitted = params[:movie].permit(:title,:rating,:release_date)
+        @movie.update!(permitted)
+         
+        flash[:notice] = "#{@movie.title} was successfully updated."
+        redirect_to movie_path(@movie)
+      end
+
+      def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        puts "testetestseteststestsetse"
+        flash[:notice] = "Movie '#{@movie.title}' deleted."
+        redirect_to movies_path
+      end
+
   end
